940ff0cccccafdab7616224d874d5dcf7ed03f68
Chris Burgess <chris@giantrobot.co.nz>
2017-04-24 16:12:49 +1200

    Add price field values & available options to $API ContributionPage.get

--- CRM/Entityextra/APIWrapper.php
+++ CRM/Entityextra/APIWrapper.php
@@ -14,11 +14,31 @@ class CRM_EntityExtra_APIWrapper implements API_Wrapper {
   public function toApiOutput($apiRequest, $result) {
     if (strtolower($apiRequest['action'] == 'get')) {
       switch (strtolower($apiRequest['entity'])) {
-        case 'ContributionPage':
-          if (!isset($result['some_random_info'])) {
-            $result['some_random_info'] = $apiRequest;
-            // $result['values'][$result['id']]['display_name_munged'] = 'MUNGE! ' . $result['values'][$result['id']]['display_name'];
-            // unset($result['values'][$result['id']]['display_name']);
+        case 'contributionpage':
+          if (isset($result['values'])) {
+            foreach ($result['values'] as &$contributionPage) {
+              if (!isset($contributionPage['price_set'])) {
+                // Returns a single integer or false.
+                if ($priceSetId = CRM_Price_BAO_PriceSet::getFor('civicrm_contribution_page', $contributionPage['id'])) {
+                  $priceSetResult = civicrm_api3('PriceSet', 'get', ['id' => $priceSetId]);
+                  if (!empty($priceSetResult['values'])) {
+                    $price_set = $priceSetResult['values'];
+                    $priceFieldResult = civicrm_api3('PriceField', 'get', ['price_set_id' => $priceSetId]);
+                    if (!empty($priceFieldResult['values'])) {
+                      $price_set[$priceSetId]['price_field'] = $priceFieldResult['values'];
+                      foreach ($priceFieldResult['values'] as $k => $priceField) {
+                        $price_set[$priceSetId]['price_field'][$k] = $priceField;
+                        $pfv_res = civicrm_api3('PriceFieldValue', 'get', ['price_field_id' => $k]);
+                        if (!empty($pfv_res['values'])) {
+                          $price_set[$priceSetId]['price_field'][$k]['values'] = $pfv_res;
+                        }
+                      }
+                    }
+                  }
+                  $result['values']['price_set'] = $price_set;
+                }
+              }
+            }
           }
 
       }
